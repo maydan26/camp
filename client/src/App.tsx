b7fd0bddc204831705bbde1eec2ddf7b30d88d14
May Dan <may@Mays-MacBook-Pro.local>
2026-01-14 01:11:51 +0200

    minimal react app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,9 @@
-import FlexList from "./components/FlexList/FlexList.tsx";
-import GridList from "./components/GridList/GridList.tsx";
 import "./App.scss";
-import { useState } from "react";
 
 function App() {
-  const [status, setStatus] = useState("flex");
   return (
     <div className="lists-container">
-      <button onClick={() => setStatus("flex")}>Flex</button>
-      <button onClick={() => setStatus("grid")}>Grid</button>
-      {status === "flex" ? <FlexList /> : <GridList />}
+      <h1>Hi Hadas :)</h1>
     </div>
   );
 }
