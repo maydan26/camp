c745940ce60805bbd2a827402411a18c0d312524
May Dan <may@Mays-MacBook-Pro.local>
2026-01-14 10:39:10 +0200

    added highlight

--- client/src/components/ItemsList/ItemList.tsx
+++ client/src/components/ItemsList/ItemList.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import type { Item } from "../../types/types";
-import { useEffect, useRef } from "react";
+import { useLayoutEffect, useRef } from "react";
+import "./itemList.scss";
 
 interface ItemListProps {
   itemData: Item;
@@ -14,14 +15,35 @@ const compareProps = (
 };
 
 const ItemList: React.FC<ItemListProps> = React.memo(({ itemData }) => {
+  const [isHighlighted, setIsHighlighted] = useState(false);
   const renderCounter = useRef(0);
   renderCounter.current++;
+
+  useLayoutEffect(() => {
+    if (itemData.lastUpdated) {
+      let sum = 0;
+      for (let i = 0; i < 500000000; i++) {
+        sum += Math.sqrt(i);
+      }
+
+      setIsHighlighted(true);
+      const timeoutId = setTimeout(() => {
+        setIsHighlighted(false);
+      }, 2000);
+
+      return () => {
+        console.log("clear");
+        clearTimeout(timeoutId);
+      };
+    }
+  }, [itemData.lastUpdated]);
+
   return (
-    <>
+    <div className={isHighlighted ? "item-highlighted" : "item"}>
       <p>{`item name: ${itemData.name}`}</p>
       <p>{`last update: ${itemData.lastUpdated || "-"}`}</p>
       <p>{`Number of renders: ${renderCounter.current}`}</p>
-    </>
+    </div>
   );
 }, compareProps);
 
